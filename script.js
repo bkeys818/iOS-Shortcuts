c77505527c56c192e510da03ff02194d21fd82f5
Benjamin Keys <bkeys818@gmail.com>
2020-08-09 21:42:13 -0400

    nav view complete

--- script.js
+++ script.js
@@ -1,102 +1,97 @@
-var body = {}
-
-let id = ''
-// var complexity = 0 // for later
+// Instead of linking to new documents, this page works by storing differnt body elements for each page. Then, when a new page is triggered, the appropiate body element is pulled from storage and set as the document's body.
 
 // Get dictionary from searchParams
 let url = new URL(window.location.href)
-if (url.searchParams.has("dictionary")) {
-    let dicSting = url.searchParams.get("dictionary")
-    let dictionary = JSON.parse(dicSting)
+if (url.searchParams.has('dictionary') == false) {
+    throw new Error('Please use search params to pass in an object with the key "dictionary".')
+}
+const input = JSON.parse(url.searchParams.get('dictionary'))
 
-    // Create basic metadata table
-    var basicTable = document.createElement('TABLE');
-    var basicCaption = basicTable.createCaption()
-    basicCaption.innerHTML = 'Basic Metadata'
-    for (key in dictionary) {
-        if (typeof dictionary[key] != "object" && Array.isArray(dictionary[key]) == false) {
-            let newRow = createRow(key, dictionary[key])
-            basicTable.appendChild(newRow)
-            delete dictionary[key]
-        }
+// Main Page
+const topBody = document.createElement('body')
+// Create basic metadata table
+let basicTable = createTable('Basic Metadata')
+for (let key in input) {
+    let value = input[key]
+    if (typeof value != 'object' && Array.isArray(value) == false) {
+        let row = createRow(key, value)
+        basicTable.appendChild(row)
+        delete input[key]
     }
-    document.body.appendChild(basicTable)
+}
+topBody.appendChild(basicTable)
 
-    // Create tables for complex objects 
-    for (key in dictionary) {
-        id = key
-        var table = document.createElement('TABLE');
-        var caption = table.createCaption()
-        caption.innerHTML = key
-
-        createTable(table, dictionary[key])
-        document.body.appendChild(table)
-    }
-} else {
-    document.body.innerHTML = '<h1">No input was passed in.</h1>'
+// Create tables for complex objects & all other pages
+for (let objTitle in input) {
+    let obj = input[objTitle]
+    let objTable = buildOutTable(objTitle, obj, `(input['${objTitle}'])`, objTitle)
+    topBody.appendChild(objTable)
 }
 
-let l1_body = document.body
+// Set body to main page
+document.body = topBody
 
 
 /*** FUNCTIONS ***/
-// Function to create table for every item in object using levels for objects inside of objects
-function createTable(table, dictionary1) {
-    for (key1 in dictionary1) {
-        row += `-${key1}`
+// Create table and fill it with rows for each item in obj. If value of item is another obj, the row will contain an arrow and clicking on it will switch to a new body containing the new obj. This new body is stored as the value of the item.
+function buildOutTable(header, obj, path, navSource) {
+    let table = createTable(header)
+    for (let key in obj) {
+        let value = obj[key]
+        if (typeof value != 'object' && Array.isArray(value) == false) {
 
-        let value1 = dictionary1[key1]
-
-        if (typeof value1 == "object" && Array.isArray(value1) != true) {
-
-            let row = createRow(key1, '<svg xmlns="http://www.w3.org/2000/svg" width="26.24711" height="44.8988" viewBox="0 0 26.24711 44.8988"><path class="a" d="M25.00194,25.25027,5.90234,43.90261a3.37483,3.37483,0,0,1-2.416.99609A3.46,3.46,0,0,1,.0001,41.46514h0L0,41.4378a3.62628,3.62628,0,0,1,1.07031-2.5411L18.00394,22.4358,1.07031,6.00027A3.56343,3.56343,0,0,1,0,3.46121,3.45879,3.45879,0,0,1,3.45651.00015h0l.02982.00012A3.26711,3.26711,0,0,1,5.87694.99636l19.125,18.65034a3.77477,3.77477,0,0,1,0,5.60352Z" fill="#C7C7CC"/></svg>')
-
-            body[row.id] = document.createElement('body')
-
-            var table = document.createElement('TABLE');
-
-            var caption = table.createCaption()
-            caption.innerHTML = (dictionary[key])[row.id]
-
-            for (key2 in value1) {
-
-                let row = createRow(key2, value1[key2])
-
-                table.appendChild(row)
-
-                l2_body.appendChild(table)
-            }
-
-            row.onclick = function () {
-
-                document.body = body[row.id]
-            }
-
-
+            let row = createRow(key, value)
             table.appendChild(row)
 
         } else {
-            let row = createRow(key1, value1)
+
+            let row = createRow(key, '<svg xmlns="http://www.w3.org/2000/svg" width="25.426" height="44.177" style="padding-top:3px"><path d="M24.404 24.355L5.03 43.329a2.958 2.958 0 11-4.134-4.232l17.432-17.01L.896 5.08A2.984 2.984 0 010 2.963 2.923 2.923 0 012.963 0 2.88 2.88 0 015.03.847l19.374 18.95a3.249 3.249 0 011.022 2.29 2.965 2.965 0 01-1.022 2.268z" fill="#C7C7CC"/></svg>')
             table.appendChild(row)
-            delete value1
+
+            let thisPath = `(${path}['${key}'])`
+            row.id = thisPath
+
+            let newTable = buildOutTable(key, value, thisPath)
+
+            let newBody = document.createElement('body')
+
+            newBody.appendChild(createNavLink(navSource))
+            newBody.appendChild(newTable)
+
+            var f1 = new Function('b', 'a', `${row.id}=a;`)
+            f1(input, newBody)
+
+            row.onclick = function () {
+                var f2 = new Function(`document.body=${row.id}`)
+                f2()
+            }
         }
     }
-    delete dictionary1
+    return table
+}
+
+function createTable(header) {
+    let table = document.createElement('table')
+    let caption = table.createCaption()
+    caption.innerHTML = header
+    return table
 }
 
 function createRow(key, value) {
-    var row = document.createElement('tr')
-
-    var spacerCell = row.insertCell(0)
+    let row = document.createElement('tr')
+    let spacerCell = row.insertCell(0)
     spacerCell.className = 'spacer'
-
-    var keyCell = row.insertCell(1)
+    let keyCell = row.insertCell(1)
     keyCell.className = 'key'
     keyCell.innerHTML = key
-
-    var valueCell = row.insertCell(2)
+    let valueCell = row.insertCell(2)
     valueCell.className = 'value'
     valueCell.innerHTML = value
-
     return row
+}
+
+function createNavLink(backButtonText) {
+    let header = document.createElement('header')
+    header.innerHTML = `<div><svg xmlns="http://www.w3.org/2000/svg" width="18" height="29"><path d="M1.93 13.477l9.118-8.918a1.335 1.335 0 01.972-.398 1.383 1.383 0 011.395 1.394 1.453 1.453 0 01-.422.996l-8.215 8.004 8.215 8.004a1.43 1.43 0 01.422.996 1.383 1.383 0 01-1.395 1.395 1.335 1.335 0 01-.972-.398l-9.118-8.93a1.395 1.395 0 01-.48-1.067 1.437 1.437 0 01.48-1.078z" fill="#007AFF"/><path fill="none" d="M0 0h18v29H0z"/></svg><p>${backButtonText}</p></div>`
+    return header
 }
